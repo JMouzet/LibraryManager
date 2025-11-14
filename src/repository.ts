05cc4887cbc375a575db4b8f72c1bcdd7ab73e08
JMouzet <julien.mouzet@coda-student.school>
2025-11-14 16:17:22 +0100

    Add repository logic

--- src/repository.ts
+++ src/repository.ts
@@ -3,8 +3,20 @@
 ///////////
 
 export class Repository<T extends { id: number }> {
-    add (item: T): void
-    getAll(): T[]
-    findById(id: number): T | undefined
-    removeById(id: number): boolean
+    private data: T[] = [];
+
+    add(item: T): void {
+        this.data.push(item);
+    }
+    getAll(): T[] {
+        return this.data;
+    }
+    findById(id: number): T | undefined {
+        return this.data.find(i => i.id === id);
+    }
+    removeById(id: number): boolean {
+        const size = this.data.length;
+        this.data.splice(this.data.findIndex(i => i.id === id), 1);
+        return size != this.data.length;
+    }
 }
