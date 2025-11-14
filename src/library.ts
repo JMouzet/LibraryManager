7a1e222c8b0e252cff2b3a84f66dde09ebfdb0bc
Dylserker <levant.dylan@gmail.com>
2025-11-14 15:09:23 +0100

    library.ts class + main.ts logique en commentaire

---
+++ src/library.ts
@@ -0,0 +1,64 @@
+import { Book } from "./book";
+import { Author } from "./author";
+
+export class Library {
+    private books: Book[] = [];
+
+    addBook(book: Book): void {
+        this.books.push(book);
+    }
+
+    listAvailable(): Book[] {
+        return this.books.filter(book => book.available === true);
+    }
+
+    findBookById(id: number): Book | undefined {
+        return this.books.find(book => book.id === id);
+    }
+}
+
+// Ajout de 3 livres au système
+const library = new Library();
+
+const author1: Author = {
+    id: 1,
+    name: "Victor Hugo",
+    birthYear: 1802
+};
+
+const author2: Author = {
+    id: 2,
+    name: "Jules Verne",
+    birthYear: 1828
+};
+
+const author3: Author = {
+    id: 3,
+    name: "Albert Camus",
+    birthYear: 1913
+};
+
+library.addBook({
+    id: 1,
+    title: "Les Misérables",
+    author: author1,
+    available: true,
+    category: ["novel"]
+});
+
+library.addBook({
+    id: 2,
+    title: "Vingt mille lieues sous les mers",
+    author: author2,
+    available: true,
+    category: ["novel", "science"]
+});
+
+library.addBook({
+    id: 3,
+    title: "L'Étranger",
+    author: author3,
+    available: true,
+    category: ["novel"]
+});
+
